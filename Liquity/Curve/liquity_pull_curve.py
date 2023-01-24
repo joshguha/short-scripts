60fe2674ab812f88019b108a6dd9158781682c23
Lor3mipsvm <52577544+Lor3mipsvm@users.noreply.github.com>
2023-01-24 00:06:50 +0000

    Update liquity_pull_curve.py

--- Liquity/Curve/liquity_pull_curve.py
+++ Liquity/Curve/liquity_pull_curve.py
@@ -7,15 +7,14 @@ import pickle as pkl
 pool_address = "0xed279fdd11ca84beef15af5d39bb4d4bee23f0ca"
 endpoint = "https://api.thegraph.com/subgraphs/name/convex-community/volume-mainnet"
 PAGE_SIZE = 1000
-end_block = 12399631 # end_block = 12399631
-max_iterations = 4100  # 4100 - blocks in univ3 range / 1000 + buffer
+end_block = 12399631
+max_iterations = 50
 counter = 0
 
 # Define the initial query
 query = """
 {
-  swapEvents(where: {pool: """ + '"' + pool_address + '"' + """} orderBy: timestamp orderDirection:desc, first: """ + str(PAGE_SIZE) + """) {
-    id
+  swapEvents(where: {pool: """ + '"' + pool_address + '"' + """} orderBy: block orderDirection:desc, first: """ + str(PAGE_SIZE) + """) {
     block
     timestamp
     tokenSold
@@ -29,23 +28,24 @@ query = """
 # Execute the initial query
 response = requests.post(endpoint, json={'query': query})
 data = json.loads(response.text)
-count = len(data["data"]["swapEvents"])
+found_swaps = len(data["data"]["swapEvents"])
 
 # Initialize the last ID variable & the dataframe
-lastID = data["data"]["swapEvents"][count-1]["id"]
+last_block = data["data"]["swapEvents"][-1]["block"]
+print(last_block)
 df = pd.json_normalize(data['data']['swapEvents'])
 
 # Use a while loop to repeatedly query the API if additional data in relevant time period
 has_more = True
 while has_more:
     try:
-        if int(data["data"]["swapEvents"][count-1]["block"]) <= end_block or count < PAGE_SIZE:
+        if int(data["data"]["swapEvents"][0]["block"]) <= end_block or found_swaps < PAGE_SIZE:
             has_more = False
             break
         
         query = """
         {
-        swapEvents(where: {pool: """ + '"' + pool_address + '"' + """} orderBy: timestamp orderDirection:desc, first: """ + str(PAGE_SIZE) + """, lastID: """ + '"' + lastID + '"' + """) {
+        swapEvents(where: {pool: """ + '"' + pool_address + '"' + """, block_lt: """ + str(last_block) + """} orderBy: block orderDirection:desc, first: """ + str(PAGE_SIZE) + """) {
             id
             block
             timestamp
@@ -56,16 +56,18 @@ while has_more:
         }
         }
         """
-        
-        response = requests.post(endpoint, json={'query': query})
+
+        response = requests.post(endpoint, json={'query': query}, timeout=3)
         data = json.loads(response.text)
-        count = len(data["data"]["swapEvents"]) # update count for pagination
+        found_swaps = len(data["data"]["swapEvents"])
         
-        if int(data["data"]["swapEvents"][count-1]["block"]) <= end_block or count < PAGE_SIZE:
+        if int(data["data"]["swapEvents"][-1]["block"]) <= end_block or found_swaps < PAGE_SIZE:
             has_more = False
             break
         
-        lastID = data["data"]["swapEvents"][count-1]["id"]
+        last_block = data["data"]["swapEvents"][-1]["block"]
+        print(last_block)
+
         df = pd.concat([df, pd.json_normalize(data['data']['swapEvents'])], ignore_index=True)
         df.reset_index(drop=True,inplace=True)
         
@@ -73,15 +75,27 @@ while has_more:
         counter +=1
         print(counter)
         if counter >= max_iterations:
-            print(df)
+            
+            df.drop_duplicates(subset='id', keep='first', inplace=True)
+            df.reset_index(inplace=True)
+            
+            print(len(df))
             df.to_feather('/Users/jonas/Workspace/Local/Drop/results/raw_liquity_logs_curve.feather')
             raise Exception("Maximum number of iterations reached. File saved.")
 
     except KeyError as e:
-        print("An error occurred:", e)
+        print("An error occurred:", e, "File saved.")
+        
+        df.drop_duplicates(subset='id', keep='first', inplace=True)
+        df.reset_index(inplace=True)
+
+        print(len(df))
         df.to_feather('/Users/jonas/Workspace/Local/Drop/results/raw_liquity_logs_curve.feather')
      
 # output
+df.drop_duplicates(subset='id', keep='first', inplace=True)
+df.reset_index(inplace=True)
+
+print(len(df))
+df.to_feather("/Users/jonas/Workspace/Local/Drop/results/raw_liquity_logs_curve.feather")
 print('the loop actually exited correctly')
-df.to_feather("/Users/jonas/Workspace/Local/Drop/results/raw_liquity_logs_curve_backup.feather")
-print('success')
